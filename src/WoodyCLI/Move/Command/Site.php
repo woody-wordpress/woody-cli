de37076110fb79faa2e9bc529931cc83143fdd9b
Sébastien Chandonay <sebastien.chandonay@raccourci.fr>
2024-10-02 16:19:04 +0200

    [edit] move site

--- src/WoodyCLI/Move/Command/Site.php
+++ src/WoodyCLI/Move/Command/Site.php
@@ -7,9 +7,11 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 use Symfony\Component\Finder\Finder;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * Site
@@ -77,31 +79,34 @@ class Site extends WoodyCommand
         $this->setTargetCore($input->getOption('core'));
 
         $this->consoleH1($this->output, sprintf("Déplacement du site '%s' du core '%s' vers le core '%s'", $this->site_key, $this->site_core_key, $this->target_core_key));
+        $this->consoleH3($this->output, 'Note : si une erreur survient au cours de ce processus et que le serveur se retrouve dans un état non souhaité, vous pouvez relancer un puppet_apply pour le remettre dans son état initial.');
+
+        $helper = $this->getHelper('question');
+        $question = new ConfirmationQuestion('Voulez-vous vraiment déplacer ce site ? ', false);
+        if (!$helper->ask($input, $output, $question)) {
+            return WoodyCommand::SUCCESS;
+        }
 
         $this->consoleH2($this->output, 'Changement de la configuration nginx');
-        // $this->change_nginx();
+        $this->change_nginx();
 
-        // Déplacer le .env entre les cores (ex: /home/admin/woody_01/current/config/sites/woody-sandbox/.env)
         $this->consoleH2($this->output, 'Déplacement de la configuration du site');
-        // $this->move_site_env();
+        $this->move_site_env();
 
-        // Modifier le cron dans (ex: /etc/cron.d/wp_woody-sandbox)
         $this->consoleH2($this->output, 'Modification des crons');
-        // $this->change_cron();
+        $this->change_cron();
 
-        // Creer le symlink du theme dans le nouveau core
         $this->consoleH2($this->output, 'Déplacement du thème dans le nouveau core');
         $this->move_theme();
 
-        // Modifier le yaml dans /home/admin/www/woody_status/config/woody_01.yml
-        $this->consoleH2($this->output, 'Changement de la configuration dans woody-status');
+        $this->consoleH2($this->output, 'Changement de la configuration woody_status');
+        $this->change_woody_status_config();
 
         $this->consoleH2($this->output, 'Nginx service reload');
-        // TODO uncomment following line
-        // $this->exec('sudo service nginx reload');
+        $this->reload_nginx();
 
-        $this->consoleH1($this->output, '!!! Pour que ce changement soit persistant, vous devez modifier la configuration de Puppet');
-        // N'oubliez pas de répercuter ça dans puppet
+        $this->consoleH1($this->output, 'Déplacement terminé');
+        $this->consoleH2($this->output, '!!! Pour que ce changement de core soit persistant, vous devez modifier la configuration de Puppet !!!');
 
         return WoodyCommand::SUCCESS;
     }
@@ -146,27 +151,56 @@ class Site extends WoodyCommand
             $this->consoleExec($this->output, $cmd);
             $this->exec($cmd);
 
-            $symlink_path = str_replace('/etc/nginx/sites-available/', '/etc/nginx/sites-enabled/', $path);
             $this->consoleH3($this->output, "mise à jour du symlink");
+            $symlink_path = str_replace('/etc/nginx/sites-available/', '/etc/nginx/sites-enabled/', $path);
             $cmd = sprintf("sudo rm -f %s", $symlink_path);
             $this->consoleExec($this->output, $cmd);
             $this->exec($cmd);
+
             $cmd = sprintf("sudo ln -s %s /etc/nginx/sites-enabled/", $new_path);
             $this->consoleExec($this->output, $cmd);
             $this->exec($cmd);
+
+            $target_core_nginx_log_path = sprintf("/var/log/nginx/%s/%s", $this->target_core_key, $this->site_key);
+            if (!$this->fs->exists($target_core_nginx_log_path)) {
+                $this->consoleH3($this->output, "création du dossier de log");
+                $cmd = sprintf("sudo mkdir -p %s", $target_core_nginx_log_path);
+                $this->consoleExec($this->output, $cmd);
+                $this->exec($cmd);
+            }
+
+            $target_core_nginx_access_log_path = sprintf("%s/access.log", $target_core_nginx_log_path);
+            if (!$this->fs->exists($target_core_nginx_access_log_path)) {
+                $this->consoleH3($this->output, "création du access.log");
+                $cmd = sprintf("sudo touch %s", $target_core_nginx_access_log_path);
+                $this->consoleExec($this->output, $cmd);
+                $this->exec($cmd);
+            }
+
+            $target_core_nginx_error_log_path = sprintf("%s/error.log", $target_core_nginx_log_path);
+            if (!$this->fs->exists($target_core_nginx_error_log_path)) {
+                $this->consoleH3($this->output, "création du error.log");
+                $cmd = sprintf("sudo touch %s", $target_core_nginx_error_log_path);
+                $this->consoleExec($this->output, $cmd);
+                $this->exec($cmd);
+            }
         }
     }
 
     /**
      * Move site's .env into new targeted core
      */
-    protected function move_site_env () {
-        $env_dir = sprintf('%s/config/sites/%s/', $this->site_core_path, $this->site_key);
-        if (!$this->fs->exists($env_dir)) {
+    protected function move_site_env() {
+        $site_env_dir = sprintf('%s/config/sites/%s/', $this->site_core_path, $this->site_key);
+        if (!$this->fs->exists($site_env_dir)) {
             throw new \RuntimeException("Le .env du site n'existe pas");
         }
         $target_env_dir = sprintf('%s/config/sites/', $this->target_core_path);
-        $cmd = sprintf("sudo mv %s %s", $env_dir, $target_env_dir);
+        if ($this->fs->exists($target_env_dir)) {
+            $this->consoleH3($this->output, sprintf("Avertissement : une configuration est déjà existante à l'emplacement '%s' pour le site - elle n'a pas été remplacée.", $target_env_dir));
+            return;
+        }
+        $cmd = sprintf("sudo mv %s %s", $site_env_dir, $target_env_dir);
         $this->consoleExec($this->output, $cmd);
         $this->exec($cmd);
     }
@@ -188,17 +222,55 @@ class Site extends WoodyCommand
     /**
      * Move site's theme into new targeted core
      */
-    protected function move_theme () {
-        // TODO WIP
-        /*
-        $theme_dir = sprintf('%s/config/sites/%s/', $this->site_core_path, $this->site_key);
-        if (!$this->fs->exists($env_dir)) {
-            throw new \RuntimeException("Le .env du site n'existe pas");
-        }
-        $target_env_dir = sprintf('%s/config/sites/', $this->target_core_path);
-        $cmd = sprintf("sudo mv %s %s", $env_dir, $target_env_dir);
+    protected function move_theme() {
+        $cmd = sprintf("sudo rm -f %s/web/app/themes/%s", $this->site_core_path, $this->site_key);
         $this->consoleExec($this->output, $cmd);
         $this->exec($cmd);
-        */
+
+        $theme_symlink_source = sprintf("/home/admin/www/themes/%s/current", $this->site_key);
+        $theme_symlink_target = sprintf("%s/web/app/themes/%s", $this->target_core_path, $this->site_key);
+        if ($this->fs->exists($theme_symlink_target)) {
+            $this->consoleH3($this->output, sprintf("Avertissement : le symlink '%s' existe déjà - veuillez vous assurer qu'il pointe bien la ressource '%s'", $theme_symlink_target, $theme_symlink_source));
+        } else {
+            $cmd = sprintf("sudo ln -s %s %s", $theme_symlink_source, $theme_symlink_target);
+            $this->consoleExec($this->output, $cmd);
+            $this->exec($cmd);
+        }
+    }
+
+    /**
+     * Change woody_status config for new targeted core
+     */
+    protected function change_woody_status_config() {
+
+        $site_core_yml_path = sprintf('/home/admin/www/woody_status/shared/config/%s.yml', $this->site_core_key);
+        $this->consoleH3($this->output, sprintf("retrait du site de la configuration '%s'", $site_core_yml_path));
+        if ($this->fs->exists($site_core_yml_path)) {
+            $site_core_config = Yaml::parseFile($site_core_yml_path);
+            unset($site_core_config['sites'][array_search($this->site_key, $site_core_config['sites'])]);
+            file_put_contents($site_core_yml_path, Yaml::dump($site_core_config));
+        } else {
+            $this->consoleH3($this->output, sprintf("Avertissement : le fichier de configuration woody_status du site n'existe pas à cet endroit : %s", $site_core_yml_path));
+        }
+
+        $target_core_yml_path = sprintf('/home/admin/www/woody_status/shared/config/%s.yml', $this->target_core_key);
+        $this->consoleH3($this->output, sprintf("ajout du site à la configuration '%s'", $target_core_yml_path));
+        if ($this->fs->exists($target_core_yml_path)) {
+            $target_core_config = Yaml::parseFile($target_core_yml_path);
+            $target_core_config['sites'][] = $this->site_key;
+            sort($target_core_config['sites']);
+            file_put_contents($target_core_yml_path, Yaml::dump($target_core_config));
+        } else {
+            $this->consoleH3($this->output, sprintf("Avertissement : le fichier de configuration woody_status du site n'existe pas à cet endroit : %s", $target_core_yml_path));
+        }
+    }
+
+    /**
+     * Reload nginx to get new configuration
+     */
+    protected function reload_nginx() {
+        $cmd = 'sudo service nginx reload';
+        $this->consoleExec($this->output, $cmd);
+        $this->exec($cmd);
     }
 }
